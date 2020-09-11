4aac8cac5d392b766084639e9f75f919c6a470aa
rayers96 <rayers96@gmail.com>
2020-09-11 11:52:26 -0500

    Implemented Clean Swift architecture

--- iOSweepTests/iOSweepTests.swift
+++ iOSweepTests/iOSweepTests.swift
@@ -10,32 +10,9 @@ import XCTest
 @testable import iOSweep
 
 class iOSweepTests: XCTestCase {
-  
-  var game : Game! = nil
-  
+
     override func setUpWithError() throws {
-      game = Game()
     }
     override func tearDownWithError() throws {
-      game = nil
-    }
-  
-    func testFlip() throws {
-      let x = Int.random(in: 0...9)
-      let y = Int.random(in: 0...9)
-      let response1 = game.get()
-      XCTAssert(response1.textureMap[x][y] == 9)
-      let response2 = game.flip(x: x, y: y)
-      XCTAssert((0...8).contains(response2.textureMap[x][y]))
-    }
-    func testFlag() throws {
-      let x = Int.random(in: 0...9)
-      let y = Int.random(in: 0...9)
-      let response1 = game.get()
-      XCTAssert(response1.textureMap[x][y] != 10)
-      let response2 = game.toggleFlag(x: x, y: y)
-      XCTAssert(response2.textureMap[x][y] == 10)
-      let response3 = game.toggleFlag(x: x, y: y)
-      XCTAssert(response3.textureMap[x][y] != 10)
     }
 }
